efca6f8075d95e6f6cbf906e5bbc0982957f830c
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2008-11-20 22:51:09 +0000

    Moved to tau-tagging exclusive DB record.
    Requires:
    
    V05-03-00 CondFormats/DataRecord
    V00-02-00 CondCore/BTauPlugins

--- test/build_mapping.sh
+++ test/build_mapping.sh
@@ -3,7 +3,10 @@
 eval `scramv1 runtime -sh`
 
 pool_build_object_relational_mapping \
-	-f $CMSSW_RELEASE_BASE/src/CondFormats/PhysicsToolsObjects/xml/MVAComputerContainer_basic_0.xml \
+        -f $CMSSW_RELEASE_BASE/src/CondFormats/PhysicsToolsObjects/xml/MVAComputerContainer_basic_0.xml \
 	-d CondFormatsPhysicsToolsObjects \
 	-c sqlite_file:Example.db \
 	-u me -p mypass -info
+
+#	-f $CMSSW_BASE/src/RecoTauTag/TauTagTools/xml/TauTagMVARecordDBBuilder.xml \
+#-f $CMSSW_RELEASE_BASE/src/CondFormats/PhysicsToolsObjects/xml/MVAComputerContainer_basic_0.xml \
