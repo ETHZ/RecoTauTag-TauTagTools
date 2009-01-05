f68a2ea40f5ca5a8353be5d9fc8b8fbdca831b69
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2009-01-05 22:12:18 +0000

    Added versions of cfi/cfgs to support (hopefully) upcoming inclusion of inside out cone tau algorithm.
    Added support functions for RecoTauTag/RecoTau functionality to use entire jet as tau signal cones.

---
+++ test/BuildSignal_cfg_for_3_2_X.py
@@ -0,0 +1,120 @@
+'''
+BuildSignal_cfg.py
+Author: Evan K. Friis, UC Davis; evan.friis@cern.ch
+
+Build signal ROOT files to support TauMVA training
+
+Sequence:
+   Pythia Z->tautau (both taus decay hadronically) events
+   Simulation done w/ FastSim package
+   Particle Flow
+   Standard HighEfficiency Tau sequence
+   Tau Decay mode reconstruction 
+   MC Truth Tau DecayMode production
+   MC Truth Tau DecayMode <-> reco::PFTau matching
+   ROOT trees of discriminant outputs applied
+'''
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TauMVA")
+
+batchNumber=1
+jobNumber=1
+nEvents = 100
+rootFileOutputPath="./"
+
+#for batch running
+batchNumber=RPL_BATCH #2
+jobNumber=RPL_RUN #69
+nEvents=RPL_EVENTS #1001
+
+#get a random number from the batch/job number for reproducibility (not robust)
+import random
+random.seed(batchNumber)
+rand1=random.randint(0, 10000)
+random.seed(jobNumber)
+rand2=random.randint(0, 10000)
+baseSeed=rand1+rand2
+
+print "Seed is %i" % baseSeed
+
+process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32(nEvents) )
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    # This is to initialize the random engines of Famos
+    moduleSeeds = cms.PSet(
+        l1ParamMuons = cms.untracked.uint32(baseSeed+54525),
+        caloRecHits = cms.untracked.uint32(baseSeed+654321),
+        MuonSimHits = cms.untracked.uint32(baseSeed+97531),
+        muonCSCDigis = cms.untracked.uint32(baseSeed+525432),
+        muonDTDigis = cms.untracked.uint32(baseSeed+67673876),
+        famosSimHits = cms.untracked.uint32(baseSeed+13579),
+        paramMuons = cms.untracked.uint32(baseSeed+54525),
+        famosPileUp = cms.untracked.uint32(baseSeed+918273),
+        VtxSmeared = cms.untracked.uint32(baseSeed+123456789),
+        muonRPCDigis = cms.untracked.uint32(baseSeed+524964),
+        siTrackerGaussianSmearingRecHits = cms.untracked.uint32(baseSeed+24680)
+    ),
+    # This is to initialize the random engine of the source
+    sourceSeed = cms.untracked.uint32(baseSeed+123456789)
+)
+
+#Z to tau tau hadronic only pythia source
+#process.load("RecoTauTag/TauTagTools/ZtoTauHadronic_cfi")
+process.load("RecoTauTag/TauTagTools/DiTaus_cfi")
+
+# Common inputs, with fake conditions
+process.load("FastSimulation.Configuration.CommonInputsFake_cff")
+# Famos sequences
+process.load("FastSimulation.Configuration.FamosSequences_cff")
+# Parametrized magnetic field (new mapping, 4.0 and 3.8T)
+#process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
+
+process.famosPileUp.PileUpSimulator.averageNumber = 0.0    
+# You may not want to simulate everything for your study
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+
+#material effects
+process.famosSimHits.PairProduction     = cms.bool(False)
+process.famosSimHits.Bremsstrahlung     = cms.bool(False)
+process.famosSimHits.EnergyLoss         = cms.bool(False)
+process.famosSimHits.NuclearInteraction = cms.bool(False)
+
+
+# Simulation sequence
+process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
+
+process.main = cms.Sequence(process.genParticles*process.famosWithParticleFlow)
+
+process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")                       # Standard Tau sequences
+process.load("RecoTauTag.RecoTau.PFRecoTauDecayModeDeteriminator_cfi")          # Reconstructs decay mode and associates (via AssociationVector) to PFTaus
+process.load("RecoTauTag.TauTagTools.TruthTauDecayModeProducer_cfi")            # Builds PFTauDecayMode objects from visible taus/gen jets
+process.load("RecoTauTag.TauTagTools.TauRecoTruthMatchers_cfi")                 # Matches RECO PFTaus to truth PFTauDecayModes
+process.load("RecoTauTag.TauTagTools.TauMVATrainer_cfi")                        # Builds MVA training input root trees from matching
+process.load("RecoTauTag.TauTagTools.TauMVADiscriminator_cfi")
+
+process.tauMVATrainerSignal.outputRootFileName="%s/output_%i_%i.root" % (rootFileOutputPath, batchNumber, jobNumber)
+
+
+process.p1 = cms.Path(process.main*
+                      process.vertexreco*
+                      process.PFTau*
+#                      process.pfRecoTauProducerInsideOut*
+                      process.pfTauDecayModeInsideOut*
+                      process.pfTauDecayModeInsideOutTiny*
+                      process.pfTauDecayModeHighEfficiency*
+                      process.makeMC*
+                      process.matchMCTaus*
+                      process.tauMVATrainerSignal)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    info_RPL_BATCH_RPL_RUN = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING'),
+    ),
+    destinations = cms.untracked.vstring('info_RPL_BATCH_RPL_RUN')
+)
+# Make the job crash in case of missing product
+process.options = cms.untracked.PSet( Rethrow = cms.untracked.vstring('ProductNotFound') )
